729456ba7c163e9fb2e5f85a8b3acb00e5425afb
0mjs <dev@mattjs.me>
2025-04-17 01:05:48 +0100

    f

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ type Post struct {
 var (
 	posts        []Post
 	postsMap     = make(map[string]Post)
-	weeklyStatus = "This blog is built with Zinc, an API framework for Go."
+	weeklyStatus = "Hmm..."
 )
 
 // Custom template functions
